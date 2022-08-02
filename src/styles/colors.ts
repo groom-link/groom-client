fb8f54585c83987038980786754397bb75d19a92
HyunoChoi <soonitoon@gmail.com>
2022-08-02 20:01:32 +0900

    Feat/i7 (#8)
    * feat: Add color palette
    
    * feat: Add custom font
    
    * chore: project setting

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,1 +1,21 @@
-export const colors = {} as const;
+export const colors = {
+  grayScale: {
+    white: '#FFFFFF',
+    gray01: '#F2F4F8',
+    gray03: '#DDE1E6',
+    gray04: '#A2A9B0',
+    gray05: '#697077',
+    gray06: '#343A3F',
+    gray07: '#121619'
+  },
+  mainColor: {
+    purple: '#AE71FF',
+    purpleLight: '#DABDFF',
+    navy: '#50539F',
+    navyLight: '#8689C7'
+  },
+  etcColor: {
+    alertRed: '#FF655C',
+    dim: '#343A3F' // alpha 0.6
+  }
+} as const;
