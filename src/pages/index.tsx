46f2ba2b5834b56cf051edc906a21a68afa7eb28
Hyuno Choi <soonitoon@gmail.com>
2022-08-02 16:21:11 +0900

    chore: Set env files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 
-import { REDIRECT_URL } from '../constants/kakao';
-
-const KAKAO_JAVASCRIPT_KEY = '248c0f042143239e8aff41927befcf0a';
-
 const Login = () => {
   useEffect(() => {
-    window.Kakao.init(KAKAO_JAVASCRIPT_KEY);
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY as string);
   }, []);
 
   const handleLogin = () => {
-    Kakao.Auth.authorize({ redirectUri: REDIRECT_URL });
+    Kakao.Auth.authorize({
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL
+    });
   };
 
   return (
