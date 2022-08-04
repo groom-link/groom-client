90ddf0aca289c1211b942011492e6dded1d06fc4
Hyuno Choi <soonitoon@gmail.com>
2022-08-04 22:29:10 +0900

    feat: Mark up Tab component

---
+++ src/components/atom/Tag/index.tsx
@@ -0,0 +1,32 @@
+import colors from '../../../styles/colors';
+import { X } from '../icons';
+import { TagBoxDiv, TagSpan } from './styled';
+
+type Props =
+  | {
+      state: 'default';
+      text: string;
+    }
+  | {
+      state: 'cancel';
+      text: string;
+      onCancel: () => void;
+    };
+
+const Tag = (props: Props) => {
+  const { state, text } = props;
+
+  return (
+    <TagBoxDiv
+      {...{ state }}
+      {...(state === 'cancel' && { onClick: props.onCancel })}
+    >
+      <TagSpan {...{ state }}>#{text}</TagSpan>
+      {state === 'cancel' && (
+        <X width="15px" height="15px" color={colors.grayScale.white} />
+      )}
+    </TagBoxDiv>
+  );
+};
+
+export default Tag;
