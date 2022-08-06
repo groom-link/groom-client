cbd0e72ff3c548f53bbdc270ed2d74e80af7d32d
HyunoChoi <soonitoon@gmail.com>
2022-08-06 18:03:12 +0900

    Top Nav Bar 컴포넌트 추가 및 아이콘 default color 변경 (#22)
    * feat: Markup Top Nav Bar component
    
    * refactor: Change default color of icons

---
+++ src/components/molecules/TopNavBar/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+import ArrowLeft from '../../atom/icons/ArrowLeft';
+import Setting from '../../atom/icons/Setting';
+import { NavBoxDiv } from './styled';
+
+type Props =
+  | {
+      backURL: string;
+      setting: false;
+    }
+  | {
+      backURL: string;
+      settingURL: string;
+      setting: true;
+    };
+
+const Tab = (prop: Props) => {
+  const { setting, backURL } = prop;
+
+  return (
+    <NavBoxDiv>
+      <Link href={backURL}>
+        <ArrowLeft width="24px" height="24px" />
+      </Link>
+      {setting && (
+        <Link href={prop.settingURL}>
+          <Setting width="24px" height="24px" />
+        </Link>
+      )}
+    </NavBoxDiv>
+  );
+};
+
+export default Tab;
