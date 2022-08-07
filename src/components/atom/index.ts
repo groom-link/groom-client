a9b230005e63fcfb282e21ae3edff016f149f673
Hyuno Choi <soonitoon@gmail.com>
2022-08-07 23:04:04 +0900

    refactor: Apply IconButton component to Stepper component

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -1,3 +1,4 @@
 export { default as Avatar } from './Avatar';
+export { default as IconButton } from './IconButton';
 export { default as Label } from './Label';
 export { default as Tag } from './Tag';
