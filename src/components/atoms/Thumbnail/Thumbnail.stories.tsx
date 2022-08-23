346a585c14de915acdf71c683cc67e564232834f
Hyuno Choi <soonitoon@gmail.com>
2022-08-23 17:35:32 +0900

    feat: Markup Thumbnail component

---
+++ src/components/atoms/Thumbnail/Thumbnail.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Thumbnail from './index';
+
+export default {
+  title: 'atoms/Thumbnail',
+  component: Thumbnail
+} as ComponentMeta<typeof Thumbnail>;
+
+export const Primary: ComponentStory<typeof Thumbnail> = ({
+  size = 'small',
+  company = '버거킹',
+  menu = '와퍼주니어세트',
+  price = '6,900',
+  isActive = false,
+  onClick
+}) => <Thumbnail {...{ size, company, menu, price, isActive, onClick }} />;
