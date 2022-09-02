5c218fb71edf7d310e11f741d02b75a88beb7b66
HyunoChoi <soonitoon@gmail.com>
2022-09-02 16:25:52 +0900

    feat: Markup GPSButton component

---
+++ src/components/molecules/GPSButton/GPSButton.stories.tsx
@@ -0,0 +1,11 @@
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import GPSButton from './index';
+
+export default {
+  title: 'molecules/GPSButton',
+  component: GPSButton
+} as ComponentMeta<typeof GPSButton>;
+
+export const Primary: ComponentStory<typeof GPSButton> = () => <GPSButton />;
