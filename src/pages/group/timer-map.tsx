2485e561ca98d674c508f8c1d0e1513d090cce95
Hyuno Choi <soonitoon@gmail.com>
2022-09-12 17:15:23 +0900

    feat: Make destination marker on the kakao map

--- src/pages/group/timer-map.tsx
+++ src/pages/group/timer-map.tsx
@@ -4,7 +4,12 @@ import styled from '@emotion/styled';
 import { DEMO_PROFILE_IMAGE_URL } from '../../__mocks__';
 import { TimerPopup, TopNavBar } from '../../components/molecules';
 import colors from '../../styles/colors';
-import { renderKakapMap, renderProfileMarker } from '../../utils/kakaoMaps';
+import {
+  getDestinationMarkerMarkup,
+  getProfileMarkerMarkup,
+  renderCustomOverlay,
+  renderKakapMap
+} from '../../utils/kakaoMaps';
 
 const Map = styled.div`
   box-sizing: border-box;
@@ -29,7 +34,20 @@ const TimerMap = () => {
   }) => {
     window.kakao.maps.load(() => {
       const map = renderKakapMap(latitude, longitude, mapRef);
-      renderProfileMarker(latitude, longitude, DEMO_PROFILE_IMAGE_URL, map);
+      renderCustomOverlay(
+        latitude,
+        longitude,
+        getProfileMarkerMarkup(DEMO_PROFILE_IMAGE_URL),
+        map
+      );
+      renderCustomOverlay(
+        // TODO: 실제 약속 장소의 위도, 경도 연동하기.
+        // 사용자의 프로필 마커와 도착 장소 마커를 한 눈에 볼 수 있도록 임시로 설정해놓았습니다.
+        latitude + 0.001,
+        longitude + 0.001,
+        getDestinationMarkerMarkup(),
+        map
+      );
     });
   };
 
--- src/utils/kakaoMaps.ts
+++ src/utils/kakaoMaps.ts
@@ -2,7 +2,7 @@ import { RefObject } from 'react';
 
 import colors from '../styles/colors';
 
-const getProfileMarkerMarkup = (profileURL: string) => `
+export const getProfileMarkerMarkup = (profileURL: string) => `
   <style>
     .marker {
       display: flex;
@@ -26,6 +26,98 @@ const getProfileMarkerMarkup = (profileURL: string) => `
   </div>
 `;
 
+export const getDestinationMarkerMarkup = () => `
+    <style>
+      .pin {
+        width: 30px;
+        height: 30px;
+        border-radius: 50% 50% 50% 0;
+        background: ${colors.mainColor.purple};
+        position: absolute;
+        transform: rotate(-45deg);
+        left: 50%;
+        top: 50%;
+        margin: -20px 0 0 -20px;
+        animation-name: bounce;
+        animation-fill-mode: both;
+        animation-duration: 1s;
+      }
+      .pin:after {
+        content: "";
+        width: 14px;
+        height: 14px;
+        margin: 8px 0 0 8px;
+        background: ${colors.grayScale.white};
+        position: absolute;
+        border-radius: 50%;
+      }
+      .pulse {
+        background: rgba(0, 0, 0, 0.2);
+        border-radius: 50%;
+        height: 14px;
+        width: 14px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin: 11px 0px 0px -12px;
+        transform: rotateX(55deg);
+        z-index: -2;
+      }
+      .pulse:after {
+        content: "";
+        border-radius: 50%;
+        height: 40px;
+        width: 40px;
+        position: absolute;
+        margin: -13px 0 0 -13px;
+        animation: pulsate 1s ease-out;
+        animation-iteration-count: infinite;
+        opacity: 0;
+        filter: alpha(opacity=0);
+        box-shadow: 0 0 1px 2px ${colors.mainColor.purple};
+        animation-delay: 1.1s;
+      }
+
+      @keyframes pulsate {
+        0% {
+          transform: scale(0.1, 0.1);
+          opacity: 0;
+          filter: alpha(opacity=0);
+        }
+        50% {
+          opacity: 1;
+          filter: none;
+        }
+        100% {
+          transform: scale(1.2, 1.2);
+          opacity: 0;
+          filter: alpha(opacity=0);
+        }
+      }
+
+      @keyframes bounce {
+        0% {
+          opacity: 0;
+          filter: alpha(opacity=0);
+          transform: translateY(-2000px) rotate(-45deg);
+        }
+        60% {
+          opacity: 1;
+          filter: none;
+          transform: translateY(30px) rotate(-45deg);
+        }
+        80% {
+          transform: translateY(-10px) rotate(-45deg);
+        }
+        100% {
+          transform: translateY(0) rotate(-45deg);
+        }
+      }
+  </style>
+  <div class='pin'></div>
+  <div class='pulse'></div>
+`;
+
 export const renderKakapMap = (
   latitude: number,
   longitude: number,
@@ -39,17 +131,17 @@ export const renderKakapMap = (
   return map;
 };
 
-export const renderProfileMarker = (
+export const renderCustomOverlay = (
   latitude: number,
   longitude: number,
-  profileURL: string,
+  content: string,
   map: any
 ) => {
   const position = new window.kakao.maps.LatLng(latitude, longitude);
   const customMarker = new window.kakao.maps.CustomOverlay({
     map: map,
     position: position,
-    content: getProfileMarkerMarkup(profileURL),
+    content,
     yAnchor: 1
   });
   return customMarker;
