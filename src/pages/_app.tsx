19b50966a023ddc8754b6dfee4279c285e7ddb44
Hyuno Choi <soonitoon@gmail.com>
2022-09-24 00:30:38 +0900

    feat: Remove unused import automatically

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/Global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 // global style, provider, recoil root, ...
