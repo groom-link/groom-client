bd108ea01428478e63fb94a1a9624a2dd128dfe5
Hyuno Choi <soonitoon@gmail.com>
2022-09-29 02:52:00 +0900

    feat: Hide profile image more than five

--- src/pages/group/home.tsx
+++ src/pages/group/home.tsx
@@ -6,6 +6,134 @@ import { TopNavBar } from '../../components/molecules';
 import colors from '../../styles/colors';
 import { medium12, semiBold16, semiBold20 } from '../../styles/typography';
 
+const MEETINGS = [
+  {
+    id: 1,
+    title: '회의 제목',
+    location: '선릉역 SWM 센터 7층 회의실',
+    date: '2022.09.01 (목)',
+    participants: [
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL
+    ]
+  },
+  {
+    id: 2,
+    title: '회의 제목',
+    location: '선릉역 SWM 센터 7층 회의실',
+    date: '2022.09.01 (목)',
+    participants: [
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL
+    ]
+  },
+  {
+    id: 3,
+    title: '회의 제목',
+    location: '선릉역 SWM 센터 7층 회의실',
+    date: '2022.09.01 (목)',
+    participants: [
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL,
+      DEMO_PROFILE_IMAGE_URL
+    ]
+  }
+];
+
+type MeetingCardProps = {
+  title: string;
+  location: string;
+  date: string;
+  participants: string[];
+};
+
+const MeetingTitle = styled.h3`
+  ${semiBold20}
+  margin-bottom: 8px;
+  color: ${colors.grayScale.gray05};
+`;
+
+const MeetingLocation = styled.span`
+  ${medium12};
+  display: block;
+  margin-bottom: 4px;
+  color: ${colors.grayScale.gray04};
+`;
+
+const MeetingDate = styled.time`
+  ${medium12}
+  color: ${colors.grayScale.gray03};
+`;
+
+const ProfileImageContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 12px;
+`;
+
+const ProfileImage = styled(Avatar)`
+  margin: 0 8px 8px 0;
+`;
+
+const MoreProfile = styled.span`
+  ${semiBold16}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+  color: ${colors.mainColor.navy};
+  background-color: ${colors.grayScale.gray01};
+`;
+
+const MeetingCard = ({
+  title,
+  location,
+  date,
+  participants
+}: MeetingCardProps) => {
+  const getParticipants = (participants: string[]) => {
+    if (participants.length > 6) {
+      return participants.slice(0, 5);
+    }
+    return participants;
+  };
+
+  return (
+    <Card>
+      <MeetingTitle>{title}</MeetingTitle>
+      <MeetingLocation>{location}</MeetingLocation>
+      <MeetingDate>{date}</MeetingDate>
+      <ProfileImageContainer>
+        {getParticipants(participants).map((participant) => (
+          <ProfileImage key={participant} src={participant} />
+        ))}
+        {participants.length > 6 && (
+          <MoreProfile>+{participants.length - 5}</MoreProfile>
+        )}
+      </ProfileImageContainer>
+    </Card>
+  );
+};
+
 const Background = styled.div`
   height: 100vh;
   background-color: ${colors.grayScale.gray01};
@@ -47,50 +175,6 @@ const Card = styled.div`
   background-color: ${colors.grayScale.white};
 `;
 
-const MeetingTitle = styled.h3`
-  ${semiBold20}
-  margin-bottom: 8px;
-  color: ${colors.grayScale.gray05};
-`;
-
-const MeetingLocation = styled.span`
-  ${medium12};
-  display: block;
-  margin-bottom: 4px;
-  color: ${colors.grayScale.gray04};
-`;
-
-const MeetingDate = styled.time`
-  ${medium12}
-  color: ${colors.grayScale.gray03};
-`;
-
-const ProfileImageContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 12px;
-`;
-
-const ProfileImage = styled(Avatar)`
-  margin: 0 8px 8px 0;
-`;
-
-const MeetingCard = () => {
-  return (
-    <Card>
-      <MeetingTitle>회의 제목</MeetingTitle>
-      <MeetingLocation>선릉역 SWM 센터 7층 회의실</MeetingLocation>
-      <MeetingDate>2022.09.01 (목)</MeetingDate>
-      <ProfileImageContainer>
-        <ProfileImage src={DEMO_PROFILE_IMAGE_URL} />
-        <ProfileImage src={DEMO_PROFILE_IMAGE_URL} />
-        <ProfileImage src={DEMO_PROFILE_IMAGE_URL} />
-        <ProfileImage src={DEMO_PROFILE_IMAGE_URL} />
-      </ProfileImageContainer>
-    </Card>
-  );
-};
-
 const Home = () => {
   return (
     <Background>
@@ -103,7 +187,9 @@ const Home = () => {
       </NavigationBox>
       <CardBox>
         <SubTitle>가까운 회의 일정</SubTitle>
-        <MeetingCard />
+        {MEETINGS.map(({ id, title, location, date, participants }) => (
+          <MeetingCard key={id} {...{ title, location, date, participants }} />
+        ))}
       </CardBox>
     </Background>
   );
