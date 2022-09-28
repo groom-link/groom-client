b88dc361ad7bdb576623c145daf2abaf01fb349e
Hyuno Choi <soonitoon@gmail.com>
2022-09-28 17:30:57 +0900

    fix: Fix side effect

--- src/pages/group/join-success.tsx
+++ src/pages/group/join-success.tsx
@@ -1,10 +1,17 @@
+import Router from 'next/router';
+
 import { GoBackHome } from '../../components/templates';
 
 const JoinSuccess = () => {
+  const handleClick = () => Router.push('/home');
+
   return (
     <GoBackHome
+      proptype="one-button"
       title="모임 가입이 완료되었습니다!"
       description="홈 화면에 새 모임이 추가되었습니다."
+      purpleButtonLabel="홈화면으로 돌아가기"
+      onPurpleButtonClick={handleClick}
     />
   );
 };
