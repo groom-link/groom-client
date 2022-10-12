1cf4c4850e211d5617ba5615320b91480ee80931
Hyuno Choi <soonitoon@gmail.com>
2022-10-12 20:56:31 +0900

    feat: Make zustand store

---
+++ src/store/newGroupInformation.ts
@@ -0,0 +1,38 @@
+import create from 'zustand';
+
+type GroupInformationState = {
+  profileImageURL: string;
+  name: string;
+  description: string;
+  tags: string[];
+  numberOfMembers: number;
+  gifticonID: string;
+  maximumNumberOfPenalty: number;
+  setProfileImageURL: (profileImageURL: string) => void;
+  setName: (name: string) => void;
+  setDescription: (description: string) => void;
+  setTags: (tags: string[]) => void;
+  setNumberOfMembers: (numberOfMembers: number) => void;
+  setGifticonID: (gifticonID: string) => void;
+  setMaximumNumberOfPenalty: (maximumNumberOfPenalty: number) => void;
+};
+
+const useNewGroupInformationStore = create<GroupInformationState>((set) => ({
+  profileImageURL: '',
+  name: '',
+  description: '',
+  tags: [],
+  numberOfMembers: 0,
+  gifticonID: '',
+  maximumNumberOfPenalty: 0,
+  setProfileImageURL: (profileImageURL: string) => set({ profileImageURL }),
+  setName: (name: string) => set({ name }),
+  setDescription: (description: string) => set({ description }),
+  setTags: (tags: string[]) => set({ tags }),
+  setNumberOfMembers: (numberOfMembers: number) => set({ numberOfMembers }),
+  setGifticonID: (gifticonID: string) => set({ gifticonID }),
+  setMaximumNumberOfPenalty: (maximumNumberOfPenalty: number) =>
+    set({ maximumNumberOfPenalty })
+}));
+
+export default useNewGroupInformationStore;
