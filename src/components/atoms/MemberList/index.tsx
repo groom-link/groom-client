7d9d88343c75d50ec99e4fcec30e6b358287d781
HyunoChoi <soonitoon@gmail.com>
2022-10-20 22:05:26 +0900

    MemberList 컴포넌트 마크업 (#169)
    * feat: Markup MemberList component
    
    * feat: Make props
    
    * fix: Change box-sizing of MemberList container to "border-box"

---
+++ src/components/atoms/MemberList/index.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+import colors from '../../../styles/colors';
+import { regular16 } from '../../../styles/typography';
+import Avatar from '../Avatar';
+
+type Props = {
+  className?: string;
+  src: string;
+  name: string;
+};
+
+const Container = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 8px 20px;
+  background-color: ${colors.grayScale.white};
+`;
+
+const Name = styled.span`
+  ${regular16}
+  margin-left: 12px;
+  color: ${colors.grayScale.gray05};
+`;
+
+const MemberList = ({ className, src, name }: Props) => {
+  return (
+    <Container className={className}>
+      <Avatar proptype="image" src={src} />
+      <Name>{name}</Name>
+    </Container>
+  );
+};
+
+export default MemberList;
