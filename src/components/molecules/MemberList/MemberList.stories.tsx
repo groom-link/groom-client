f333cfc6ca62e683a2e33f8a15b31258a85cd044
Hyuno Choi <soonitoon@gmail.com>
2022-10-27 02:40:04 +0900

    feat: Add check function to the MemberList component

--- src/components/molecules/MemberList/MemberList.stories.tsx
+++ src/components/molecules/MemberList/MemberList.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { DEMO_PROFILE_IMAGE_URL } from '../../../__mocks__';
@@ -11,5 +11,21 @@ export default {
 
 export const Primary: ComponentStory<typeof MemberList> = ({
   src = DEMO_PROFILE_IMAGE_URL,
-  name = '구성원 이름'
-}) => <MemberList {...{ src, name }} />;
+  name = '구성원 이름',
+  check = true,
+  ...props
+}) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckChange = () => setIsChecked((pre) => !pre);
+
+  if (check)
+    return (
+      <MemberList
+        onChange={handleCheckChange}
+        {...{ src, name, check, isChecked }}
+      />
+    );
+
+  return <MemberList {...{ src, name, check }} />;
+};
--- src/components/molecules/MemberList/index.tsx
+++ src/components/molecules/MemberList/index.tsx
@@ -3,14 +3,27 @@ import styled from '@emotion/styled';
 import colors from '../../../styles/colors';
 import { regular16 } from '../../../styles/typography';
 import Avatar from '../../atoms/Avatar';
+import { CheckCircle } from '../../atoms/icons';
 
-type Props = {
+type DefaultProps = {
   className?: string;
   src: string;
   name: string;
 };
 
-const Container = styled.div`
+type ListProps = {
+  check: false;
+} & DefaultProps;
+
+type CheckProps = {
+  check: true;
+  isChecked: boolean;
+  onChange: () => void;
+} & DefaultProps;
+
+type Props = ListProps | CheckProps;
+
+const Container = styled.label`
   box-sizing: border-box;
   display: flex;
   align-items: center;
@@ -25,12 +38,42 @@ const Name = styled.span`
   color: ${colors.grayScale.gray05};
 `;
 
-const MemberList = ({ className, src, name }: Props) => {
+const DummyInput = styled.input`
+  display: none;
+`;
+
+const CheckCircleStyled = styled(CheckCircle)`
+  margin-left: auto;
+`;
+
+const MemberList = ({ className, src, name, ...props }: Props) => {
+  const inputId = `${name}-check-input`;
+
   return (
-    <Container className={className}>
-      <Avatar proptype="image" src={src} />
-      <Name>{name}</Name>
-    </Container>
+    <>
+      <Container className={className} htmlFor={inputId}>
+        <Avatar proptype="image" src={src} />
+        <Name>{name}</Name>
+        {props.check && (
+          <CheckCircleStyled
+            width="36px"
+            color={
+              props.isChecked
+                ? colors.mainColor.purple
+                : colors.grayScale.gray02
+            }
+          />
+        )}
+      </Container>
+      {props.check && (
+        <DummyInput
+          id={inputId}
+          type="checkbox"
+          checked={props.isChecked}
+          onChange={props.onChange}
+        />
+      )}
+    </>
   );
 };
 
