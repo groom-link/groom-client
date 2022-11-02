d195cc9c4212735ee590d76f0feadbc91ed7f541
Hyuno Choi <soonitoon@gmail.com>
2022-11-03 01:58:09 +0900

    feat: Make axios function and custom query hook to get all room schedules

---
+++ src/hooks/api/room/schedule/searchSchedule.ts
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+
+import customAxios, { requestIntercepter } from '../../../../api/customAxios';
+
+type SearchRoomScheduleRequest = {
+  page: number;
+  size: number;
+  sort: string[];
+  userId: number;
+  roomId: number;
+  startTime: string;
+  endTime: string;
+};
+
+type SearchRoomScheduleResponse = {
+  success: boolean;
+  teamScheduleList: {
+    title: string;
+    startTime: string;
+    meetingLocation: {
+      address: string;
+      longitude: string;
+      latitude: string;
+    };
+    profiles: string[];
+  }[];
+  page: number;
+  last: boolean;
+};
+
+// access token 검증 오류가 해결될 때까지 토큰을 싣지 않고 요청.
+customAxios.interceptors.request.eject(requestIntercepter);
+
+const getSearchRoomSchedules = async (
+  query: SearchRoomScheduleRequest
+): Promise<SearchRoomScheduleResponse> => {
+  const { data } = await customAxios.get('/room/schedule/search', {
+    params: query
+  });
+  return data;
+};
+
+const useSearchRoomSchedules = (query: SearchRoomScheduleRequest) =>
+  useQuery(['searchRoomSchedules'], () => getSearchRoomSchedules(query));
+
+export default useSearchRoomSchedules;
