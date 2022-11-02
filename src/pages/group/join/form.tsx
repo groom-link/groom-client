37eec0111f6bf0c2922b7ab3341ccd3a24475d12
Hyuno Choi <soonitoon@gmail.com>
2022-11-02 22:56:57 +0900

    feat: Open modal when user click back button in join/form page

--- src/pages/group/join/form.tsx
+++ src/pages/group/join/form.tsx
@@ -2,7 +2,7 @@ import { ChangeEventHandler, useState } from 'react';
 import Router from 'next/router';
 import styled from '@emotion/styled';
 
-import { TextArea, TopNavBar } from '../../../components/molecules';
+import { Dialog, TextArea, TopNavBar } from '../../../components/molecules';
 import ButtonFooter from '../../../components/molecules/ButtonFooter';
 import colors from '../../../styles/colors';
 import { regular16, semiBold16, semiBold20 } from '../../../styles/typography';
@@ -42,40 +42,60 @@ const Money = styled.strong`
 
 const Form = () => {
   const [text, setText] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleChange: ChangeEventHandler<HTMLTextAreaElement> = ({
     target: { value }
   }) => setText(value);
 
-  const handleBackButtonClick = () => Router.push('./detail');
+  const handleBackButtonClick = () => setIsModalOpen(true);
+
+  const handleModalConfirmButtonClick = () => Router.push('./');
+
+  const handleModalCancelButtonClick = () => setIsModalOpen(false);
 
   return (
-    <Background>
-      <TopNavBar setting={false} onBackButtonClick={handleBackButtonClick} />
-      <WhiteBox>
-        <Title>자유롭게 본인을 소개해주세요!</Title>
-        <Description>
-          자신을 소개하고, 모임비를 결제하면
-          <br />
-          모임장에게 가입 신청이 완료됩니다.
-        </Description>
-        <TextArea
-          placeholder="자신을 소개해주세요."
-          value={text}
-          onChange={handleChange}
-        />
-      </WhiteBox>
-      <WhiteBox>
-        <SubTitle>모임비</SubTitle>
-        <Money>50,000원</Money>
-      </WhiteBox>
-      <ButtonFooter
-        disabled={text === ''}
-        onClick={() => Router.push('./success')}
-      >
-        모임비 결제하기
-      </ButtonFooter>
-    </Background>
+    <>
+      <Background>
+        <TopNavBar setting={false} onBackButtonClick={handleBackButtonClick} />
+        <WhiteBox>
+          <Title>자유롭게 본인을 소개해주세요!</Title>
+          <Description>
+            자신을 소개하고, 모임비를 결제하면
+            <br />
+            모임장에게 가입 신청이 완료됩니다.
+          </Description>
+          <TextArea
+            placeholder="자신을 소개해주세요."
+            value={text}
+            onChange={handleChange}
+          />
+        </WhiteBox>
+        <WhiteBox>
+          <SubTitle>모임비</SubTitle>
+          <Money>50,000원</Money>
+        </WhiteBox>
+        <ButtonFooter
+          disabled={text === ''}
+          onClick={() => Router.push('./success')}
+        >
+          모임비 결제하기
+        </ButtonFooter>
+      </Background>
+      <Dialog
+        isOpen={isModalOpen}
+        isIllustrationExists={true}
+        title="모임 신청을 취소하시겠어요?"
+        buttonType="two"
+        purpleButtonText="아니요"
+        grayButtonText="네, 취소할게요"
+        description="모임 이름: SW개발"
+        isGrayButtonDisabled={false}
+        isPurpleButtonDisabled={false}
+        onGrayButtonClick={handleModalConfirmButtonClick}
+        onPurpleButtonClick={handleModalCancelButtonClick}
+      />
+    </>
   );
 };
 
