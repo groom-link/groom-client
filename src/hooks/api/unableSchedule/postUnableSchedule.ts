ad93910fe32538c679ca759cb5a8d104e6460ca7
Hyuno Choi <soonitoon@gmail.com>
2022-11-05 00:33:20 +0900

    feat: Post unable-time when user click submit button

--- src/hooks/api/unableSchedule/postUnableSchedule.ts
+++ src/hooks/api/unableSchedule/postUnableSchedule.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 
-import customAxios from '../../../api/customAxios';
+import customAxios, { requestIntercepter } from '../../../api/customAxios';
 
 type PostUnableScheduleRequest = {
   roomId: number;
@@ -11,6 +11,9 @@ type PostUnableScheduleRequest = {
 type PostUnableSchedule = (
   body: PostUnableScheduleRequest
 ) => Promise<GroomApiResponse<PostUnableScheduleRequest>>;
+
+// access token 검증 오류가 해결될 때까지 토큰을 싣지 않고 요청.
+customAxios.interceptors.request.eject(requestIntercepter);
 
 const postUnableSchedule: PostUnableSchedule = async (body) => {
   const { data } = await customAxios.post('/room/unableSchedule', body);
