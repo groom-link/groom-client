ae313ffee4021d24d85f416a4a74211c32491cc7
Hyuno Choi <soonitoon@gmail.com>
2022-11-05 13:02:13 +0900

    feat: Make Delete feature of suggestionTimeList component

--- src/components/atoms/SuggestionTimeList/SuggestionTimeList.stories.tsx
+++ src/components/atoms/SuggestionTimeList/SuggestionTimeList.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import SuggestionTimeList from './index';
@@ -10,5 +10,27 @@ export default {
 
 export const Primary: ComponentStory<typeof SuggestionTimeList> = ({
   startTime = '2022-11-04T10:00:00:00',
-  endTime = '2022-11-05T10:23:59:00'
-}) => <SuggestionTimeList {...{ startTime, endTime }} />;
+  endTime = '2022-11-05T10:23:59:00',
+  ...props
+}) => {
+  const [isDeleteButtonExposed, setIsDeleteButtonExposed] = useState(false);
+
+  const handleListClick = () => setIsDeleteButtonExposed((pre) => !pre);
+
+  const handleDeleteButtonClick = () => alert('삭제');
+
+  if (props.type === 'link') {
+    const { href } = props;
+    return <SuggestionTimeList type="link" {...{ startTime, endTime, href }} />;
+  }
+
+  return (
+    <SuggestionTimeList
+      type="delete"
+      isDeleteButtonExposed={isDeleteButtonExposed}
+      onClick={handleListClick}
+      onDeleteButtonClick={handleDeleteButtonClick}
+      {...{ startTime, endTime }}
+    />
+  );
+};
