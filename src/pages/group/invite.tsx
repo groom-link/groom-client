a1e4644c44de7bfbb68d5482143718a344364657
HyunoChoi <soonitoon@gmail.com>
2022-11-12 01:52:00 +0900

    초대코드 탭 마크업 및 코드 불러오는 API 연동하기 (#231)
    * feat: Markup invite code page
    
    * feat: Get invite code from DB
    
    * chore: Delete unnecessary mock data

---
+++ src/pages/group/invite.tsx
@@ -0,0 +1,61 @@
+import styled from '@emotion/styled';
+
+import { GroupPage } from '../../components/templates';
+import { useRoomIdParams } from '../../hooks';
+import useGetInviteCode from '../../hooks/api/room/getInviteCode';
+import colors from '../../styles/colors';
+import { regular16, semiBold16, semiBold24 } from '../../styles/typography';
+
+const InviteCode = styled.span`
+  ${semiBold16}
+  display: block;
+  width: min-content;
+  margin: 44px auto 0;
+  padding: 14px 52.5px;
+  border-radius: 8px;
+  color: ${colors.grayScale.gray05};
+  background-color: ${colors.grayScale.white};
+`;
+
+const Title = styled.h1`
+  ${semiBold24}
+  display: block;
+  margin: 20px auto 0;
+  text-align: center;
+  color: ${colors.grayScale.gray05};
+`;
+
+const Description = styled.p`
+  ${regular16}
+  display: block;
+  margin: 8px auto 0;
+  text-align: center;
+  color: ${colors.grayScale.gray04};
+`;
+
+const Invite = () => {
+  const roomId = useRoomIdParams();
+  const {
+    data: inviteCode,
+    isLoading: isInviteCodeLoading,
+    isError: isInviteCodeError
+  } = useGetInviteCode(roomId);
+
+  if (isInviteCodeLoading) return <div>초대코드 로딩중...</div>;
+  if (isInviteCodeError) return <div>초대코드 로딩 실패</div>;
+  if (inviteCode === undefined) return <div>초대코드 오류</div>;
+
+  return (
+    <GroupPage roomId={roomId} selectedTabIndex={3}>
+      <InviteCode>{inviteCode}</InviteCode>
+      <Title>친구들을 초대하세요!</Title>
+      <Description>
+        친구들에게 초대코드를 전달해주면,
+        <br />
+        초대코드로 모임에 가입할 수 있어요.
+      </Description>
+    </GroupPage>
+  );
+};
+
+export default Invite;
--- src/pages/group/setting/member.tsx
+++ src/pages/group/setting/member.tsx
@@ -1,55 +1,17 @@
 import Router from 'next/router';
 import styled from '@emotion/styled';
 
-import { DEMO_PROFILE_IMAGE_URL } from '../../../__mocks__';
 import { TextButton } from '../../../components/atoms';
 import {
   MemberList,
   SegmentTab,
   TopNavBar
 } from '../../../components/molecules';
+import { useRoomIdParams } from '../../../hooks';
+import useGetDetailWithRoomId from '../../../hooks/api/room/getDetailWithRoomId';
+import useGetInviteCode from '../../../hooks/api/room/getInviteCode';
 import colors from '../../../styles/colors';
 import { regular16, semiBold16 } from '../../../styles/typography';
-
-const GROUP_MEMBERS_MOCK = [
-  {
-    id: 1,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  },
-  {
-    id: 2,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  },
-  {
-    id: 3,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  },
-  {
-    id: 4,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  },
-  {
-    id: 5,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  },
-  {
-    id: 6,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  },
-  {
-    id: 7,
-    src: DEMO_PROFILE_IMAGE_URL,
-    name: '구성원 이름'
-  }
-];
-
-const INVITE_CODE_MOCK = '122 321';
 
 const Background = styled.div`
   box-sizing: border-box;
@@ -91,7 +53,34 @@ const WhiteBox = styled.div`
 `;
 
 const Member = () => {
-  const handleBackButtonClick = () => Router.push('/group');
+  const roomId = useRoomIdParams();
+  const {
+    data: inviteCode,
+    isLoading: isInviteCodeLoading,
+    isError: isInviteCodeError
+  } = useGetInviteCode(roomId);
+  const {
+    data: roomDetail,
+    isLoading: isRoomDetailLoading,
+    isError: isRoomDetailError
+  } = useGetDetailWithRoomId(roomId);
+
+  const setClipboard = (text: string) => {
+    const type = 'text/plain';
+    const blob = new Blob([text], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+
+    navigator.clipboard.write(data);
+  };
+
+  const handleBackButtonClick = () => Router.push(`/group?roomId=${roomId}`);
+
+  if (isInviteCodeLoading) return <div>초대코드 로딩중...</div>;
+  if (isInviteCodeError) return <div>초대코드 로딩 실패</div>;
+  if (inviteCode === undefined) return <div>초대코드 에러</div>;
+  if (isRoomDetailLoading) return <div>방 정보 로딩중...</div>;
+  if (isRoomDetailError) return <div>방 정보 로딩 실패</div>;
+  if (roomDetail === undefined) return <div>방 정보 에러</div>;
 
   return (
     <Background>
@@ -100,28 +89,33 @@ const Member = () => {
         <SegmentTab
           leftTabLabel="모임 정보"
           rightTabLabel="구성원 관리"
-          leftTabHref="./information"
+          leftTabHref={`./information?roomId=${roomId}`}
           rightTabHref=""
           selectedTabIndex={1}
         />
       </TabContainer>
       <InviteCodeContainer>
         <CodeLabel>초대 코드</CodeLabel>
-        <InviteCode>{INVITE_CODE_MOCK}</InviteCode>
+        <InviteCode>{inviteCode}</InviteCode>
         <InviteCodePasteButton
           color="navy"
           disabled={false}
-          onClick={function (): void {
-            throw new Error('Function not implemented.');
-          }}
+          onClick={() => setClipboard(inviteCode)}
         >
           초대 코드 복사하기
         </InviteCodePasteButton>
       </InviteCodeContainer>
       <WhiteBox>
-        {GROUP_MEMBERS_MOCK.map(({ id, src, name }) => (
-          <MemberList key={id} check={false} {...{ src, name }} />
-        ))}
+        {roomDetail.roomParticipants.map(
+          ({ id, profileImageUrl, nickname }) => (
+            <MemberList
+              key={id}
+              check={false}
+              src={profileImageUrl}
+              name={nickname}
+            />
+          )
+        )}
       </WhiteBox>
     </Background>
   );
